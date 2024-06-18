f674411e5953e8174804f4d4aef6d5cae897c0d3
Tynermeister <144954238+Tynermeister@users.noreply.github.com>
2024-06-18 16:27:57 +0100

    Create homepageMatter.js

---
+++ homepageMatter.js
@@ -0,0 +1,59 @@
+const canvas = document.querySelector("#canvas-target");
+
+// module aliases
+var Engine = Matter.Engine,
+  Render = Matter.Render,
+  Runner = Matter.Runner,
+  MouseConstraint = Matter.MouseConstraint,
+  Mouse = Matter.Mouse,
+  Bodies = Matter.Bodies,
+  Composite = Matter.Composite;
+
+// create an engine
+var engine = Engine.create(),
+  world = engine.world;
+
+// create a renderer
+var render = Render.create({
+  canvas: canvas,
+  engine: engine,
+  options: {
+    background: "transparent",
+    wireframes: false,
+  },
+});
+
+// create two boxes and a ground
+var boxA = Bodies.rectangle(400, 200, 80, 80);
+var boxB = Bodies.rectangle(450, 50, 80, 80);
+var ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
+var wallL = Bodies.rectangle(0, 300, 60, 600, { isStatic: true });
+
+// add all of the bodies to the world
+Composite.add(engine.world, [boxA, boxB, ground, wallL]);
+
+// run the renderer
+Render.run(render);
+
+// create runner
+var runner = Runner.create();
+
+// run the engine
+Runner.run(runner, engine);
+
+// add mouse control
+var mouse = Mouse.create(render.canvas),
+  mouseConstraint = MouseConstraint.create(engine, {
+    mouse: mouse,
+    constraint: {
+      stiffness: 0.2,
+      render: {
+        visible: false,
+      },
+    },
+  });
+
+Composite.add(world, mouseConstraint);
+
+// keep the mouse in sync with rendering
+render.mouse = mouse;
